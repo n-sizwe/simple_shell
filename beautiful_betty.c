c7df9a507bffe61c529bf37e868ef0de558c00f7
Nsizwe <tdsimwa19@gmail.com>
2024-01-12 14:51:27 +0300

    Initial commit - Task 0 completed

---
+++ beautiful_betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * beautiful_betty - A function to illustrate Betty style
+ *
+ * Return: Always 0.
+ */
+int beautiful_betty(void)
+{
+printf("Hello, Betty! I hope my code is beautiful!\n");
+return (0);
+}
+
+
