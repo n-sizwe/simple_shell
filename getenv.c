1a8cc291df58ca9caf4220e6ce6f54f8054a3300
Ted Simwa <44122958+n-sizwe@users.noreply.github.com>
2024-01-12 19:40:52 +0300

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,5 +1,3 @@
-#include "shell.h"
-
 /**
  * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
@@ -21,32 +19,30 @@ char **get_environ(info_t *info)
  * _unsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
+ * Return: 1 on delete, 0 otherwise
  */
 int _unsetenv(info_t *info, char *var)
 {
-	list_t *node = info->env;
-	size_t i = 0;
+	list_t *node;
 	char *p;
 
-	if (!node || !var)
+	if (!info->env || !var)
 		return (0);
 
+	node = info->env;
 	while (node)
 	{
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
-			node = info->env;
-			continue;
+			info->env_changed = delete_node_at_index(&(info->env), node->index);
+			return (info->env_changed);
 		}
 		node = node->next;
-		i++;
 	}
-	return (info->env_changed);
+
+	return (0);
 }
 
 /**
@@ -56,23 +52,46 @@ int _unsetenv(info_t *info, char *var)
  *        constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
- *  Return: Always 0
+ * Return: Always 0
  */
 int _setenv(info_t *info, char *var, char *value)
 {
-	char *buf = NULL;
-	list_t *node;
-	char *p;
+	char *env_var;
 
 	if (!var || !value)
 		return (0);
 
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buf)
+	env_var = _strjoin(var, "=", value);
+	if (!env_var)
 		return (1);
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
+
+	if (update_env_var(info, var, env_var) == 0)
+	{
+		free(env_var);
+		return (0);
+	}
+
+	add_node_end(&(info->env), env_var, 0);
+	info->env_changed = 1;
+	free(env_var);
+	return (0);
+}
+
+/**
+ * update_env_var - Updates an existing environment variable.
+ * @info: Structure containing potential arguments.
+ * @var: the string env var property
+ * @env_var: the new environment variable string
+ * Return: 0 on success, 1 on failure
+ */
+int update_env_var(info_t *info, char *var, char *env_var)
+{
+	list_t *node;
+	char *p;
+
+	if (!info->env || !var || !env_var)
+		return (1);
+
 	node = info->env;
 	while (node)
 	{
@@ -80,14 +99,12 @@ int _setenv(info_t *info, char *var, char *value)
 		if (p && *p == '=')
 		{
 			free(node->str);
-			node->str = buf;
+			node->str = env_var;
 			info->env_changed = 1;
 			return (0);
 		}
 		node = node->next;
 	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
-	info->env_changed = 1;
-	return (0);
+
+	return (1);
 }
